4d973b1dec9837f30f7e5f08e25b3befeb5624f2
nix_arif <nix_arif@nixs-MacBook-Pro.local>
2026-06-16 12:21:16 +0800

    image upload

--- server/products.ts
+++ server/products.ts
@@ -17,6 +17,7 @@ const s3 = new S3Client({
     accessKeyId: process.env.R2_ACCESS_KEY_ID!,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
   },
+  forcePathStyle: true,
 });
 const PRODUCT_IMAGES_BUCKET = process.env.R2_PRODUCT_IMAGES_BUCKET!;
 
